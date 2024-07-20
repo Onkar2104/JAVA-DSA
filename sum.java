eec758f9b3920616842e05c578a1e2767e3701d7
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-07-20 19:03:28 +0000

    commit of Table & Sum of first n no.

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++){
+            sum = sum + i;
+        }
+        
+        // System.out.printf("Sum of First" + n  "numbers is:" + sum);
+        System.out.printf("Sum of First %d numbers is: %d", n, sum);
+    }
+}
