82f80f66ad3373142d14acbdaee2634fa983825d
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-07-21 18:14:19 +0000

    patterns

---
+++ pattern_pyramid.java
@@ -0,0 +1,11 @@
+public class pattern_pyramid {
+    public static void main(String[] args) {
+        
+        for(int i=0; i<=5; i++){
+            for(int j=1; j<=5; j=j+2){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
