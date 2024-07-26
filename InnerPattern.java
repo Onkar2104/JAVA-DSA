0831a73e73ce4eab4467561846ffc7d96e0f77c5
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-07-26 17:53:35 +0000

    modify

--- InnerPattern.java
+++ InnerPattern.java
@@ -1,16 +1,16 @@
-// public class InnerPattern {
-//     public static void main(String[] args) {
-//         int i,j;
+public class InnerPattern {
+    public static void main(String[] args) {
+        int i,j;
 
-//         for(i=0; i<=3; i++){
+        for(i=0; i<=3; i++){
 
-//             for(j=0; j<=i; j++){
-//                 System.out.print("*");
-//             }
-//             System.out.println();
-//         }
-//     }
-// }
+            for(j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
 
 
 
@@ -26,17 +26,3 @@
 //         }
 //     }
 // }
-
-public class InnerPattern {
-
-    public static void main(String[] args) {
-        
-        for(int i=1; i<=6; i--){
-            for(int j=1; j>=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-}
-
