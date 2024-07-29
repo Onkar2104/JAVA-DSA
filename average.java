2a573e9422077b7f2a52eca0e926a0ca297d40e8
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-07-29 18:13:33 +0000

    average of three numbers

---
+++ average.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class average {
+    public static int averageOfThreeNumber(int a, int b, int c){
+
+        int average = (a + b + c)/3;
+        System.out.println("The Average is: " + average);
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of A: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter value of B: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter value of c: ");
+        int c = sc.nextInt();
+        
+        averageOfThreeNumber(a, b, c);
+    }
+}
