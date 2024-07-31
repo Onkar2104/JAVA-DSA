44e6ce1de70c8c77f4f3ae36a83a7bc9e6ef9122
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-07-31 17:24:10 +0000

    31/07

---
+++ array.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+
+public class array{
+    public static void main(String[] args) {
+        
+        int marks[] = {1, 2, 3};
+        // int marks[] = new int[3];
+        // marks[0]=90;
+        // marks[1]=80;
+        // marks[2]=70;
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
