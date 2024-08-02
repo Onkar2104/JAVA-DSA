0d04bcc2fd14cd7e2c54e8b1cc8e74a974983fa5
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-08-02 17:27:29 +0000

    String

---
+++ string.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+
+        //Strings are Immutable
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+
+        String fullName = firstName + " " + lastName;       //concatenation
+        System.out.println("Your Name is " + fullName);
+        
+        System.out.println(fullName.length());
+
+        // //charAt
+        // for(int i=0; i<=fullName.length(); i++){
+        //     System.out.println(fullName.charAt(i));
+        // }
+
+        //compare
+        if(firstName.compareTo(lastName) == 0){
+            System.out.println("Strings are equal..");
+        } else{
+            System.out.println("Strings are not equal..");
+        }
+    }
+}
